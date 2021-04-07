84e1b512ab984f1e1f9bf94a4ec47b8162033df6
kio <un19451024@gmail.com>
2021-04-08 00:10:56 +0900

    [Feat] KoreanItemInfo 구조체 추가

---
+++ Expo1900/Expo1900/KoreanItemInfo.swift
@@ -0,0 +1,22 @@
+//
+//  KoreanItemInfo.swift
+//  Expo1900
+//
+//  Created by kio on 2021/04/08.
+//
+
+import Foundation
+
+struct KoreanItemInfo: Codable {
+    let name: String
+    let imageName: String
+    let shortDescription: String
+    let description: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName = "image_name"
+        case shortDescription = "short_desc"
+        case description = "desc"
+    }
+}
