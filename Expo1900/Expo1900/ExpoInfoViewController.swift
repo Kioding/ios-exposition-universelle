fcabf288c3118e3e6605a8efcfa89c4f42e48055
kio <un19451024@gmail.com>
2021-04-15 19:08:58 +0900

    [Feat] ExpoInfoViewController 클래스 JSONDecoder 생성

--- Expo1900/Expo1900/ExpoInfoViewController.swift
+++ Expo1900/Expo1900/ExpoInfoViewController.swift
@@ -6,13 +6,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ExpoInfoViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var visitorsLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descrtionLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var expo: ExpoInfo
+
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "exposition_universelle_1900") else {
+            return
+        }
+        
+        do {
+            expo = try jsonDecoder.decode(ExpoInfo.self, from: dataAsset.data)
+            titleLabel.text = expo.title
+            visitorsLabel.text = "방문자: " + String(expo.visitors)
+            locationLabel.text = "개최지: " + expo.location
+            durationLabel.text = "기간:" + expo.duration
+            descrtionLabel.text = expo.description
+        } catch {
+            print(error.localizedDescription)
+        }
     }
-
-
 }
 
